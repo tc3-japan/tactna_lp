47075cdfa818b0a3cc779919e59c0275ec990d5c
umepy <haruya04183083@gmail.com>
2025-02-12 16:43:41 +0900

    wrap LogoMarquee component in Link for external navigation

--- src/app/components/logo_marquee.tsx
+++ src/app/components/logo_marquee.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import Marquee from "react-fast-marquee";
+import Link from "next/link";
 
 const logos = [
   "/client_logos/cli_sb.png",
@@ -18,30 +19,32 @@ const logos = [
 
 const LogoMarquee: React.FC = () => {
   return (
-    <Marquee>
-      {logos.map((logo, index) => (
-        <div key={index} className="flex-none mx-2 md:mx-4">
-          <Image
-            src={logo}
-            alt={`Logo ${index}`}
-            width={400}
-            height={400}
-            className="w-24 md:w-48 h-auto"
-          />
-        </div>
-      ))}
-      {logos.map((logo, index) => (
-        <div key={`duplicate_${index}`} className="flex-none mx-4">
-          <Image
-            src={logo}
-            alt={`Logo ${index}`}
-            width={400}
-            height={400}
-            className="w-24 md:w-48 h-auto"
-          />
-        </div>
-      ))}
-    </Marquee>
+    <Link href="https://www.tc3.co.jp/case/" target="_blank" rel="noopener">
+      <Marquee>
+        {logos.map((logo, index) => (
+          <div key={index} className="flex-none mx-2 md:mx-4">
+            <Image
+              src={logo}
+              alt={`Logo ${index}`}
+              width={400}
+              height={400}
+              className="w-24 md:w-48 h-auto"
+            />
+          </div>
+        ))}
+        {logos.map((logo, index) => (
+          <div key={`duplicate_${index}`} className="flex-none mx-4">
+            <Image
+              src={logo}
+              alt={`Logo ${index}`}
+              width={400}
+              height={400}
+              className="w-24 md:w-48 h-auto"
+            />
+          </div>
+        ))}
+      </Marquee>
+    </Link>
   );
 };
 

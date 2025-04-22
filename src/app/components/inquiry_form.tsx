b28f8ac77d410e7354de657257280bf957aebc15
umepy <haruya04183083@gmail.com>
2025-04-22 15:40:25 +0900

    feat: update HubSpotForm to load script based on locale

--- src/app/components/inquiry_form.tsx
+++ src/app/components/inquiry_form.tsx
@@ -1,24 +1,33 @@
 "use client";
 
 import { useEffect } from "react";
+import { useLocale } from "next-intl";
 
 const HubSpotForm = () => {
+  const locale = useLocale();
+
   useEffect(() => {
-    const script = document.createElement("script");
-    script.src = "https://js.hsforms.net/forms/embed/v2.js";
-    script.async = true;
-    script.onload = () => {
-      if (window.hbspt) {
-        window.hbspt.forms.create({
-          region: "na1",
-          portalId: process.env.NEXT_PUBLIC_HUBSPOT_PORTAL_ID ?? "",
-          formId: process.env.NEXT_PUBLIC_HUBSPOT_FORM_ID ?? "",
-          target: "#hubspot-form",
-        });
-      }
-    };
-    document.body.appendChild(script);
-  }, []);
+    if (locale) {
+      const script = document.createElement("script");
+      const formId =
+        locale === "ja"
+          ? process.env.NEXT_PUBLIC_HUBSPOT_FORM_ID
+          : process.env.NEXT_PUBLIC_HUBSPOT_FORM_ID_EN;
+      script.src = "https://js.hsforms.net/forms/embed/v2.js";
+      script.async = true;
+      script.onload = () => {
+        if (window.hbspt) {
+          window.hbspt.forms.create({
+            region: "na1",
+            portalId: process.env.NEXT_PUBLIC_HUBSPOT_PORTAL_ID ?? "",
+            formId: formId ?? "",
+            target: "#hubspot-form",
+          });
+        }
+      };
+      document.body.appendChild(script);
+    }
+  }, [locale]);
 
   return (
     <div

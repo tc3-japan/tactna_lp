8e1f03100c23ea2a9a569ec8f6404ea36392c426
umepy <haruya04183083@gmail.com>
2025-04-22 09:27:25 +0900

    add locale layout and home page components for internationalization support

---
+++ src/app/[locale]/layout.tsx
@@ -0,0 +1,39 @@
+import { notFound } from "next/navigation";
+import { ReactNode } from "react";
+import { setRequestLocale } from "next-intl/server";
+import "../globals.css";
+import { routing } from "@/i18n/routing";
+import { NextIntlClientProvider, hasLocale } from "next-intl";
+
+export const metadata = {
+  title: "Tactna",
+  description: "Tactna LP",
+};
+
+export function generateStaticParams() {
+  return routing.locales.map((locale) => ({ locale }));
+}
+
+export default async function LocaleLayout({
+  children,
+  params,
+}: {
+  children: ReactNode;
+  params: Promise<{ locale: string }>;
+}) {
+  const { locale } = await params;
+  if (!hasLocale(routing.locales, locale)) {
+    console.log("Locale not found:", locale);
+    notFound();
+  }
+
+  setRequestLocale(locale);
+
+  return (
+    <html lang={locale}>
+      <body>
+        <NextIntlClientProvider>{children}</NextIntlClientProvider>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/[locale]/page.tsx
@@ -0,0 +1,22 @@
+import Navbar from "../components/navbar";
+import Hero from "../components/hero";
+//import FeatureCarousel from "../components/carousel";
+import FeatureTable from "../components/feature_table";
+import Footer from "../components/footer";
+import HubSpotForm from "../components/inquiry_form";
+import Features from "../components/features";
+import VideoPlayer from "../components/video";
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <VideoPlayer />
+      <Features />
+      <FeatureTable />
+      <HubSpotForm />
+      <Footer />
+    </>
+  );
+}

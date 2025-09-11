cb2670582e21cd355570cde2504d94692bf6ac37
umepy <haruya04183083@gmail.com>
2025-09-11 11:41:43 +0900

    feat: update navbar links to support locale-based routing

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,38 +1,47 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import { useTranslations } from "next-intl";
+import { useTranslations, useLocale } from "next-intl";
 
 export default function Navbar() {
   const t = useTranslations("navbar");
+  const locale = useLocale();
   return (
     <>
       <nav className="fixed w-full bg-white shadow-md z-50">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between min-h-12 items-center">
             <div className="py-2 md:py-4">
-              <Image
-                src="/tactna_logo_2.png"
-                alt="Tactna Logo"
-                width={200}
-                height={100}
-                className="object-contain max-w-[120px] md:max-w-[160px] h-auto"
-              />
+              <Link href={`/${locale}`}>
+                <Image
+                  src="/tactna_logo_2.png"
+                  alt="Tactna Logo"
+                  width={200}
+                  height={100}
+                  className="object-contain max-w-[120px] md:max-w-[160px] h-auto cursor-pointer"
+                />
+              </Link>
             </div>
 
             <div className="ml-10 flex items-center space-x-4">
               <div className="hidden md:block">
                 <Link
-                  href="#features"
+                  href={`/${locale}#features`}
                   className="text-gray-700 hover:text-blue-600 px-3 py-2"
                 >
                   {t("features")}
                 </Link>
                 <Link
-                  href="#feature-comparison"
+                  href={`/${locale}#feature-comparison`}
                   className="text-gray-700 hover:text-blue-600 px-3 py-2"
                 >
                   {t("compare")}
+                </Link>
+                <Link
+                  href={`/${locale}/blog`}
+                  className="text-gray-700 hover:text-blue-600 px-3 py-2"
+                >
+                  {t("blog")}
                 </Link>
                 <Link
                   href="https://info.tc3.co.jp/hubfs/Tactna/tactna_a4_DL%E7%94%A8.pdf"
@@ -43,7 +52,7 @@ export default function Navbar() {
                 </Link>
               </div>
               <Button className="bg-blue-600">
-                <Link href="#inquiry">{t("contact")}</Link>
+                <Link href={`/${locale}#inquiry`}>{t("contact")}</Link>
               </Button>
             </div>
           </div>

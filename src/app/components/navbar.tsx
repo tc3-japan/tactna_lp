f236a875a9bf9eff046280bb40a63d85ad580fd7
umepy <haruya04183083@gmail.com>
2025-10-24 00:43:14 +0900

    Refactor imports to use localized Link component from i18n/navigation

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,18 +1,19 @@
+"use client";
+
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
-import Link from "next/link";
-import { useTranslations, useLocale } from "next-intl";
+import { Link } from "@/i18n/navigation";
+import { useTranslations } from "next-intl";
 
 export default function Navbar() {
   const t = useTranslations("navbar");
-  const locale = useLocale();
   return (
     <>
       <nav className="fixed w-full bg-white shadow-md z-50">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between min-h-12 items-center">
             <div className="py-2 md:py-4">
-              <Link href={`/${locale}`}>
+              <Link href="/">
                 <Image
                   src="/tactna_logo_2.png"
                   alt="Tactna Logo"
@@ -26,33 +27,34 @@ export default function Navbar() {
             <div className="ml-10 flex items-center space-x-4">
               <div className="hidden md:block">
                 <Link
-                  href={`/${locale}#features`}
+                  href="/#features"
                   className="text-gray-700 hover:text-blue-600 px-3 py-2"
                 >
                   {t("features")}
                 </Link>
                 <Link
-                  href={`/${locale}#feature-comparison`}
+                  href="/#feature-comparison"
                   className="text-gray-700 hover:text-blue-600 px-3 py-2"
                 >
                   {t("compare")}
                 </Link>
                 <Link
-                  href={`/${locale}/blog`}
+                  href="/blog"
                   className="text-gray-700 hover:text-blue-600 px-3 py-2"
                 >
                   {t("blog")}
                 </Link>
-                <Link
+                <a
                   href="https://info.tc3.co.jp/hubfs/Tactna/tactna_a4_DL%E7%94%A8.pdf"
                   target="_blank"
+                  rel="noopener noreferrer"
                   className="text-gray-700 hover:text-blue-600 px-3 py-2"
                 >
                   {t("download_material")}
-                </Link>
+                </a>
               </div>
               <Button className="bg-blue-600">
-                <Link href={`/${locale}#inquiry`}>{t("contact")}</Link>
+                <Link href="/#inquiry">{t("contact")}</Link>
               </Button>
             </div>
           </div>
